4da27157144b9056d2234048a53bea9a77ed3f42
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-02-08 00:37:19 +0900

    oauth 세션

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.example.demo.config;
 
-import com.example.demo.service.impl.OAuth2UserServiceImpl;
+import com.example.demo.oauth.ClientRegistrationRepo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
@@ -8,16 +8,19 @@ import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final OAuth2UserServiceImpl customOAuth2UserService;
+    private final OAuth2UserService customOAuth2UserService;
+    private final ClientRegistrationRepo customClientRegistrationRepo;
+    public SecurityConfig(OAuth2UserService customOAuth2UserService, ClientRegistrationRepo customClientRegistrationRepo) {
 
-    public SecurityConfig(OAuth2UserServiceImpl customOAuth2UserService) {
         this.customOAuth2UserService = customOAuth2UserService;
+        this.customClientRegistrationRepo = customClientRegistrationRepo;
     }
 
     @Bean
@@ -48,6 +51,7 @@ public class SecurityConfig {
                         .anyRequest().denyAll()
                 )
                 .oauth2Login((oauth2) -> oauth2
+                        .clientRegistrationRepository(customClientRegistrationRepo.clientRegistrationRepository())
                         .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
                                 .userService(customOAuth2UserService)
                         )
